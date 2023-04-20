cd598bacfd47aff7c2a8ae20e1bfa99a56dfff4b
toughyear <rajivsingh430@gmail.com>
2023-04-20 13:00:41 +0530

    add grid

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const ctx = imageCanvas.getContext("2d");
 const downloadJSON = document.getElementById("downloadJSON");
 const imageContainer = document.getElementById("imageContainer");
 
+const gridSizeInput = document.getElementById("gridSize");
+const showGridCheckbox = document.getElementById("showGrid");
+
+function drawGrid(gridSize) {
+  if (!showGridCheckbox.checked) return;
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.4)";
+  for (let x = 0; x <= imageCanvas.width; x += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, imageCanvas.height);
+    ctx.stroke();
+  }
+  for (let y = 0; y <= imageCanvas.height; y += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(imageCanvas.width, y);
+    ctx.stroke();
+  }
+}
+
 let image = new Image();
 let isDrawing = false;
 let rectangles = [];
@@ -19,6 +39,7 @@ imageFile.addEventListener("change", (e) => {
       imageCanvas.width = image.width;
       imageCanvas.height = image.height;
       ctx.drawImage(image, 0, 0);
+      drawGrid(parseInt(gridSizeInput.value));
     };
   };
 
@@ -37,7 +58,17 @@ imageCanvas.addEventListener("mousedown", (e) => {
 });
 
 imageCanvas.addEventListener("mousemove", (e) => {
-  if (!isDrawing) return;
+  if (!isDrawing) {
+    const x = e.clientX - imageCanvas.getBoundingClientRect().left;
+    const y = e.clientY - imageCanvas.getBoundingClientRect().top;
+    ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+    ctx.drawImage(image, 0, 0);
+    drawGrid(parseInt(gridSizeInput.value));
+    ctx.fillStyle = "black";
+    ctx.fillText(`(${x}, ${y})`, x + 5, y - 5);
+    return;
+  }
+
   const rect = rectangles[rectangles.length - 1];
   rect.width = e.clientX - imageCanvas.getBoundingClientRect().left - rect.x;
   rect.height = e.clientY - imageCanvas.getBoundingClientRect().top - rect.y;
@@ -75,4 +106,16 @@ downloadJSON.addEventListener("click", () => {
   link.href = url;
   link.download = "rectangles.json";
   link.click();
+});
+
+gridSizeInput.addEventListener("change", () => {
+  ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+  ctx.drawImage(image, 0, 0);
+  drawGrid(parseInt(gridSizeInput.value));
+});
+
+showGridCheckbox.addEventListener("change", () => {
+  ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+  ctx.drawImage(image, 0, 0);
+  drawGrid(parseInt(gridSizeInput.value));
 });
